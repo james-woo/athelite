f141ede2f81125404a476064053088207ff49249
James Woo <jamesjaywoo@gmail.com>
2016-05-26 14:56:40 -0700

    Update Injection.java

--- app/src/mock/java/com.jameswoo.athelite/Injection.java
+++ app/src/mock/java/com.jameswoo.athelite/Injection.java
@@ -2,9 +2,6 @@ package com.jameswoo.athelite;
 
 import android.content.Context;
 
-import com.jameswoo.athelite.data.workouts.FakeWorkouts;
-import com.jameswoo.athelite.data.settings.FakeSettings;
-
 public class Injection {
 
 }
