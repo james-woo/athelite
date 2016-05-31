481e685995d922f28b58379e075d502129969b18
james-woo <jamesjaywoo@gmail.com>
2016-05-31 02:48:20 -0700

    Update workout unit test

--- app/src/test/java/com/athelite/JsonUtilTest.java
+++ app/src/test/java/com/athelite/JsonUtilTest.java
@@ -1,7 +1,26 @@
 package com.athelite;
 
-/**
- * Created by James Woo on 5/31/2016.
- */
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+import static org.mockito.Mockito.*;
+
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.athelite.Model.Exercise;
+import com.athelite.Model.ExerciseSet;
+import com.athelite.Model.WorkoutPlan;
+import com.athelite.Util.JsonSerializer;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+@RunWith(MockitoJUnitRunner.class)
 public class JsonUtilTest {
+
 }
