60b2f2ca328de1ff777f29d89162456df78272f1
James Woo <james.woo@safe.com>
2016-06-08 17:28:19 -0700

    fixes

--- app/release.sh
+++ app/release.sh
@@ -7,20 +7,13 @@ ARR=(${VERSION_NAME//./ })
 VERSION_CODE=$((  1000000*${ARR[0]} + 1000*${ARR[1]} + ${ARR[2]}  ))
 
 git stash
-git checkout develop -b release/$VERSION_NAME
 
 sed -i \
 	-e "s/versionName \".*\"/versionName \"$VERSION_NAME\"/" \
 	-e "s/versionCode .*/versionCode $VERSION_CODE/" \
 	$DIR/build.gradle
 
-git commit -a -m "Update version in build.gradle to $VERSION_NAME."
-git checkout master
-git merge --no-ff --no-edit release/$VERSION_NAME
-git push
 git tag $VERSION_NAME
 git checkout develop
-git merge --no-ff --no-edit release/$VERSION_NAME
-git branch -d release/$VERSION_NAME
 git push
 git push --tags
