0bf0e8bf939ed310935255253c5826d99ce9709d
james-woo <jamesjaywoo@gmail.com>
2016-06-11 00:46:24 -0700

    1.0.4

--- app/release.sh
+++ app/release.sh
@@ -6,13 +6,12 @@ VERSION_NAME=$1
 ARR=(${VERSION_NAME//./ })
 VERSION_CODE=$((  1000000*${ARR[0]} + 1000*${ARR[1]} + ${ARR[2]}  ))
 
-git stash
-
 sed -i \
 	-e "s/versionName \".*\"/versionName \"$VERSION_NAME\"/" \
 	-e "s/versionCode .*/versionCode $VERSION_CODE/" \
 	$DIR/build.gradle
 
+git add --all
+git commit -m $VERSION_NAME
 git tag $VERSION_NAME
-git push
 git push --tags
